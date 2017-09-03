779b248012d7934212d8a6d74f3e443d4ce7e46d
Felipe Torrejón <felipetorrejon@live.cl>
2017-09-02 22:10:38 -0300

    cambio gradual de color de fondo

--- scripts.js
+++ scripts.js
@@ -41,7 +41,7 @@ function proporcion() {
 
   var t = new Date(estaciones[indice].termino);
 
-  for( var i = 0; i < estaciones.length; i++) {
+  for (var i = 0; i < estaciones.length; i++) {
     if (Date.now() < t.getTime()) {
       indice = i;
       break
@@ -63,8 +63,86 @@ function proporcion() {
   textoEstacion.innerHTML = season.nombre;
   textoFaltante.innerHTML = salida;
 
+  fondo("bg", season.loQueQueda, season.nombre);
 }
 
-function iniciar(){
-  setInterval(proporcion,1000);
+function fondo(id, proporcion, estacion) {
+  var colores = [
+    {
+      nombre: "Invierno",
+      rojo: 255,
+      verde: 204,
+      azul: 51
+    },
+    {
+      nombre: "Primavera",
+      rojo: 204,
+      verde: 153,
+      azul: 102
+    },
+    {
+      nombre: "Verano",
+      rojo: 102,
+      verde: 153,
+      azul: 204
+    },
+    {
+      nombre: "Otoño",
+      rojo: 255,
+      verde: 102,
+      azul: 102
+    }
+  ];
+
+  var elegido = false;
+  var indice;
+  if (proporcion < 0.5) {
+    indice = 3;
+  } else {
+    indice = 4;
+  };
+
+  var color1 = "";
+  var color2 = "";
+
+  while (!elegido) {
+    var i = indice % 4;
+    if (colores[i].nombre == estacion) {
+      elegido = true;
+      r1 = colores[i].rojo;
+      r2 = colores[i+1].rojo;
+      v1 = colores[i].verde;
+      v2 = colores[i+1].verde;
+      a1 = colores[i].azul;
+      a2 = colores[i+1].azul;
+    };
+    indice++;
+  }
+
+  var color, rojo, verde, azul;
+
+  rojo = mezcla(proporcion, r1, r2);
+  verde = mezcla(proporcion, v1, v2);
+  azul = mezcla(proporcion, a1, a2);
+
+  color = "rgb(" + rojo + ", " + verde + ", " + azul + ")";
+
+  document.getElementById(id).style.backgroundColor = color;
+}
+
+function mezcla(proporcion, valor1, valor2) {
+  var p;
+  if (proporcion < 0.5) {
+    p = proporcion - 0.5;
+    valor = Math.floor( valor1 * p + valor2 * (1 - p)).toString();
+  } else {
+    p = proporcion + 0.5;
+    valor = Math.floor( valor2 * p + valor1 * (1 - p)).toString();
+  };
+  return valor;
+}
+
+function iniciar() {
+  proporcion();
+  setInterval(proporcion,100);
 }
